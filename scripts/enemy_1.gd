0d3150b87814db6c37e9ae3b22b1ffa5c982de26
Melissa Leite <melleitemelissa@gmail.com>
2026-06-04 19:51:26 -0300

    Add files via upload

---
+++ scripts/enemy_1.gd
@@ -0,0 +1,34 @@
+extends CharacterBody2D
+
+@export var velocidade = 100
+
+@onready var player = $"../player"
+@onready var body = $AnimatedSprite2D
+
+func _physics_process(delta):
+
+	if !player:
+		return
+
+	var diferenca = player.global_position - global_position
+
+	# Escolhe apenas uma das 4 direções
+	if abs(diferenca.x) > abs(diferenca.y):
+
+		velocity = Vector2(sign(diferenca.x), 0) * velocidade
+
+		if velocity.x > 0:
+			body.play("right")
+		else:
+			body.play("left")
+
+	else:
+
+		velocity = Vector2(0, sign(diferenca.y)) * velocidade
+
+		if velocity.y > 0:
+			body.play("down")
+		else:
+			body.play("up")
+
+	move_and_slide()
